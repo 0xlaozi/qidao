81160b3148793141560e0d57fa18cbad91a386a1
Lao Zi <0xdaozi@gmail.com>
2021-05-03 08:04:30 -0500

    ganache local testing

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -6,7 +6,7 @@ import "hardhat-typechain";
 const config: HardhatUserConfig = {
   defaultNetwork: "hardhat",
   solidity: {
-    compilers: [{ version: "0.5.2", settings: {
+    compilers: [{ version: "0.5.5", settings: {
       optimizer: {
         enabled: true,
         runs: 1000
@@ -18,12 +18,22 @@ const config: HardhatUserConfig = {
         runs: 1000
       }
     }
+  },{ version: "0.8.0", settings: {
+      optimizer: {
+        enabled: true,
+        runs: 1000
+      }
+    }
   }],
   },
   networks: {
     hardhat: {},
     mumbai: {
       url: `https://rpc-mumbai.maticvigil.com/`,
+      accounts: [String(process.env.MATIC_KEY)],
+    },
+    mainnet: {
+      url: `https://rpc-mainnet.maticvigil.com/`,
       accounts: [String(process.env.MATIC_KEY)],
     },
   },
